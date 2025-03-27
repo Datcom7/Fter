784be5afbb25439da8eaccba325a5ba159488d2a
Datcom7 <1roma2002271@gmail.com>
2025-03-27 15:49:19 +0300

    ET

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ArtemGay from './ArtemGay';
+
+function App() {
+  return (
+    <div className="App">
+      <ArtemGay name = {'Rae'}/>
+    </div>
+  );
+}
+
+export default App;
